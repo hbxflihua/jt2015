4d92333bf14cdcfbc85eed314f4cfdb911978dd4
hbxflihua <lihua_java@163.com>
2015-07-13 13:48:12 +0800

    add log
    add take time log!

--- src/jt2015/Test.java
+++ src/jt2015/Test.java
@@ -3,10 +3,13 @@ package jt2015;
 public class Test {
 	
 	public static void main(String[] args) {
-		
+		long t1 = System.currentTimeMillis()；
 		System.out.println("START");
 		System.out.println(System.currentTimeMillis());
 		System.out.println("END!");
+		long t2 = System.currentTimeMillis();
+		
+		System.out.println("take time:"+(t2-t1)+"ms!");
 		
 	}
 

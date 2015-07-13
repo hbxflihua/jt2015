069afd7628a2b16ca2027290af6e25f5235d31b5
hbxflihua <lihua_java@163.com>
2015-07-13 15:18:07 +0800

    c2

--- src/jt2015/Test.java
+++ src/jt2015/Test.java
@@ -3,7 +3,7 @@ package jt2015;
 public class Test {
 	
 	public static void main(String[] args) {
-		long t1 = System.currentTimeMillis()；
+		long t1 = System.currentTimeMillis();
 		System.out.println("START");
 		System.out.println(System.currentTimeMillis());
 		System.out.println("END!");

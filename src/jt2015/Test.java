cf0596e1794be9dda391b8366bebd3cadfc534bc
hbxflihua <lihua_java@163.com>
2015-07-13 13:43:10 +0800

    C1

---
+++ src/jt2015/Test.java
@@ -0,0 +1,13 @@
+package jt2015;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("START");
+		System.out.println(System.currentTimeMillis());
+		System.out.println("END!");
+		
+	}
+
+}

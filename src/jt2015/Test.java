781212aaa2f0e897d2780f7723093f5e588a21ec
hbxflihua <lihua_java@163.com>
2015-07-16 14:20:23 +0800

    Update Test.java

--- src/jt2015/Test.java
+++ src/jt2015/Test.java
@@ -3,15 +3,9 @@ package jt2015;
 public class Test {
 	
 	public static void main(String[] args) {
-		long t1 = System.currentTimeMillis();
+		
 		System.out.println("START");
-		System.out.println(System.currentTimeMillis());
-		System.out.println("END!");
-		long t2 = System.currentTimeMillis();
-		
-		System.out.println("take time:"+(t2-t1)+"ms!");
-		
-		System.out.println("read me !!! 001");
+		System.out.println("END");
 		
 	}
 
